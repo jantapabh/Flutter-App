0855a0c76f8ae682b30ceae2418fd4031cae877e
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-13 00:02:32 +0700

    add commit

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-//Colors
-
+// Colors
 const kBackgroundColor = Color(0xFFFEFEFE);
 const kTitleTextColor = Color(0xFF303030);
 const kBodyTextColor = Color(0xFF4B4B4B);
@@ -13,17 +12,13 @@ const kPrimaryColor = Color(0xFF3382CC);
 final kShadowColor = Color(0xFFB7B7B7).withOpacity(.16);
 final kActiveShadowColor = Color(0xFF4056C6).withOpacity(.15);
 
-//Text Style
-
-const kHeadTextStyle = TextStyle(
+// Text Style
+const kHeadingTextStyle = TextStyle(
   fontSize: 22,
   fontWeight: FontWeight.w600,
 );
 
-const kSubTextStyle = TextStyle(
-  fontSize: 16,
-  color: kTextLightColor
-);
+const kSubTextStyle = TextStyle(fontSize: 16, color: kTextLightColor);
 
 const kTitleTextstyle = TextStyle(
   fontSize: 18,
