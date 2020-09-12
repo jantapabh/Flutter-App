0b72f9bc9d6dcad0af23b4181ceee051e917054f
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-12 16:50:31 +0700

    add commit home

---
+++ lib/home.dart
@@ -0,0 +1,13 @@
+import 'package:projectapp/main.dart';
+import 'package:flutter/material.dart';
+
+// ใช้ stateless ในการทำงาน
+class HomePage extends StatelessWidget{
+
+  @override
+
+  Widget build(BuildContext context){
+
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,15 @@
 //Text widget
 import 'package:flutter/material.dart';
+
+//import home.dart
+import 'package:projectapp/home.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override 
   Widget build(BuildContext context){
-    return MaterialApp(
-      title: "Test Text widget",
-      home: Scaffold(
-        appBar: AppBar(title: Text("Text widget"),),
-        backgroundColor: Colors.yellow,
-        body: Container(
-          color: Colors.white,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            textBaseline: TextBaseline.alphabetic,
-            children: <Widget>[
-              Text(
-                "Hey!",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontFamily: "Futura",
-                  color: Colors.red
-                )),
-              Text(
-                "Hey!",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontFamily: "Futura",
-                  color: Colors.green
-                )), 
-              Text(
-                "Hey!",
-                style: TextStyle(
-                  fontSize: 50,
-                  fontFamily: "Futura",
-                  color: Colors.blue
-                )),                                
-                ],
-          ),
-        ),
-      ),
-    );
+   
+    return MaterialApp(home: HomePage(),);
   }
 }
