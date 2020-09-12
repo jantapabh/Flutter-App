2669344497d87acb460d7e377cb4820cbe4f5a37
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-12 17:01:46 +0700

    add Propoties

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 //Text widget
-import 'package:flutter/pages/material.dart';
+import 'package:flutter/material.dart';
 
 //import home.dart
 import 'package:projectapp/pages/home.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
-    return MaterialApp(home: HomePage(),);
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      title: 'My First App',
+    );
   }
+}
+
+//การสรา้ง class
+
+class Test {
+  String str; //string
+  int num; //number
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,13 +1,12 @@
-
 import 'package:flutter/material.dart';
 
 // ใช้ stateless ในการทำงาน
 // scaffold เป็นโครงสร้างของ Program
-class HomePage extends StatelessWidget{
-
+class HomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-
-    return Scaffold();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Jan'),),
+    );
   }
 }
